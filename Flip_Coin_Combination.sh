dfb26685df152b606dbbbed0b61d7a20b3ae7623
Milind_77 <milinddhote77@gmail.com>
2021-05-10 18:01:40 +0530

    Added: UC-2_Singlet_Combination_in_a_Dictionary

--- Flip_Coin_Combination.sh
+++ Flip_Coin_Combination.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+#UserCase_2
+hcount=0
+tcount=0
+declare -A singlet
+singlet=([H]=0 [T]=0)
+echo "---------- Wecome to FlipCoin Combination Program -----------"
+function singletheadTailGenerate(){
+	rn=$((RANDOM%2))
+       	if [ $rn -eq 0 ]
+       	then
+              	tcount=$(($tcount+1))
+       	else
+               	hcount=$(($hcount+1))
+       	fi
+}
 
+function singletCombination(){	
+	while [ $hcount -ne 21 -a $tcount -ne 21 ]
+	do
+		singletheadTailGenerate
+	done
+	singlet[H]=$hcount
+	singlet[T]=$tcount
+}
+singletCombination
+echo  " ${singlet[@]}"
+echo  " Total = $((singlet[H] + singlet[T]))"
+echo  " Heads Percentage  = $(( (singlet[H] * 100 )/ (singlet[H] + singlet[T]) ))"
+echo  " Tails Percentage  = $(( (singlet[T] * 100 )/ (singlet[H] + singlet[T]) ))"
