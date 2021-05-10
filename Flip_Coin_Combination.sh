f0beb6c8e270c2cf596a95d896d2867a88f4a8a6
Milind_77 <milinddhote77@gmail.com>
2021-05-10 17:48:05 +0530

    Added: UC-1_Head_Tails_Random_Coin

--- Flip_Coin_Combination.sh
+++ Flip_Coin_Combination.sh
@@ -1,1 +1,16 @@
-
+#!/bin/bash
+#UserCase_1
+hcount=0
+tcount=0
+echo "---------Wecome to Flip Coin Combination Program----------"
+function headTailGenerate(){
+rn=$((RANDOM%2))
+	if [ $rn -eq 0 ]
+   then
+     	tcount=$(($tcount+1))
+   else
+   	hcount=$(($hcount+1))
+   fi
+}
+headTailGenerate
+echo $hcount $tcount
